ebe0981c2ac5ac625271ee38dacaa3acc8f46ff7
Eric Cristo <ericcristo2@gmail.com>
2023-06-01 17:03:28 +0100

    revendo alguns conceitos

---
+++ quarta_parte_aprendizado/comparacoes.js
@@ -0,0 +1,13 @@
+// == comparacao implicita    
+
+const numero = 5;
+const texto = "5";
+
+console.log( numero == texto) //true == here we can test the value 
+console.log( numero === texto) // false === here we can test the value and the type~
+
+//typeof
+console.log(typeof numero)
+console.log(typeof texto)
+
+// we saw that in "segunda_parte_aprendizado"
