63d5a32cc42b9dd69b803c81d6963a1d14e1e3fd
Eric Cristo <ericcristo2@gmail.com>
2023-06-01 17:27:40 +0100

    operadores ternario

---
+++ quarta_parte_aprendizado/ternario.js
@@ -0,0 +1,12 @@
+// operador ternario like if else
+
+const minimunAge = 18;
+const ageClient = 16;
+
+//if ( ageClient >= minimunAge){
+//    console.log("beer")
+//}else{
+//    console.log("juice")
+//}
+
+console.log(ageClient >= minimunAge ? "beer" : "juice")
