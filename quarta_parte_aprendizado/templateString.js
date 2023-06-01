024ac18996279dab56e882bd29aed739db24c37b
Eric Cristo <ericcristo2@gmail.com>
2023-06-01 17:43:25 +0100

    outra forma de concatenar

---
+++ quarta_parte_aprendizado/templateString.js
@@ -0,0 +1,16 @@
+const name = "Eric";
+const age = 2023-2002;
+const birthPlace = "Rio de Janeiro"
+
+//const introduction = "hello, my name is " + name + 
+//", my age is " +  age + 
+//", i was born in " + birthPlace;
+
+
+//template string
+const introduction = `Hello my name is ${name},
+ my age is ${age},
+  i was born in ${birthPlace}`; // we need use this " ` "
+
+
+console.log(introduction)
