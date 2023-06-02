fcbc2082193295ca85d09d6eb2d95e2ac94ba41b
Eric Cristo <ericcristo2@gmail.com>
2023-06-02 16:12:36 +0100

    funções

--- quinta_parte_aprendizado/funcoes.js
+++ quinta_parte_aprendizado/funcoes.js
@@ -1,3 +1,25 @@
-let x = "";
-console.log(x)
-x = "oi";
+//let x = "";
+//console.log(x)
+//x = "oi";
+
+// DECLARAÇÃO DE FUNÇÃO
+
+// 1) declare the function
+                      //2 -> valor de soma()
+function writeText (text) {
+    console.log(text)
+}
+
+// 2) execute the function
+
+writeText(soma());
+writeText("another text");
+
+// three forms to write a function
+
+function soma(){
+    //others code
+    return 2 + 2; // need be the last line to the function
+}
+
+//console.log(soma())
