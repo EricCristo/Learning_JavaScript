d22c68fb27e40c2ed16e27b7caf72b1d22ee2e5a
Eric Cristo <ericcristo2@gmail.com>
2023-06-02 16:50:56 +0100

    estudo do que sao parametros

---
+++ quinta_parte_aprendizado/parametros.js
@@ -0,0 +1,29 @@
+//parametros de funcao
+
+//function soma(number1, number2) {
+//    return number1 + number2;//
+//}
+
+//console.log(soma(2, 2))
+//console.log(soma(12, 452))
+//console.log(soma(2, 285))
+
+// parametro x argumento 
+
+//ordem dos parametros
+
+// function nameAge(name, age) {
+    // return `my name is ${name} and im ${age}`; 
+// }
+
+// console.log(nameAge(eric, 20))
+
+function soma(number1, number2) {
+    return number1 + number2;//
+}
+
+function multiplica(number1, number2) {
+    return number1 * number2;
+}
+
+console.log(multiplica(soma(4, 5), soma(3, 3)))
