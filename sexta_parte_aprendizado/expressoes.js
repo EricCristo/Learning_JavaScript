1a0715d62c7d9530660ea327ea18a1e244e90ab6
Eric Cristo <ericcristo2@gmail.com>
2023-06-05 16:41:38 +0100

    expressoes

---
+++ sexta_parte_aprendizado/expressoes.js
@@ -0,0 +1,27 @@
+// declaracao de funcao
+
+function myFunction(param) {
+    console.log(param)
+}
+
+myFunction("param")
+
+
+//expressão de funcao 
+
+const soma = function(num1, num2)
+ {return num1 + num2}
+
+console.log(soma(1,3))
+
+
+// diferrences 
+console.log(introduce())
+
+function introduce() {
+    return "hii";
+}
+ //hoisting
+console.log(soma(4, 5))
+
+const soma2 = function(num1, num2) {return num1 +num2}
